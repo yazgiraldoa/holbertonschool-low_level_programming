34fdea8fa6beafaf5c805fb044409e655f39da1f
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-06-18 16:39:35 -0500

    add programm that prints from 0 to 9

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints from 0 to 9 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
