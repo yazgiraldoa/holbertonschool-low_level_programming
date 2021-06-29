8576f9ff27775d4e74b243380c56932669f7b77a
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-06-28 23:30:45 -0500

    add function that prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+* _puts - function that prints a string
+*
+* @str: pointer
+*/
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
