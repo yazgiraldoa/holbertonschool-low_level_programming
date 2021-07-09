0d705ac42831c10b307183fd993e47be806294c0
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-07-09 12:04:52 -0500

    add header file

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+#endif
