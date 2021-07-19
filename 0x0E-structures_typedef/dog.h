1fc62df66323bafa4a27d34e170414d4e6ff8a74
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-07-19 17:19:22 -0500

    add prototype and typdef to header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,12 +9,14 @@
  * Description: struct that describe some characteristics of a dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+/*PROTOTYPES*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif
