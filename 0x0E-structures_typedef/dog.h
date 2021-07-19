243096090e86ac3c8b37aad4139e3f2b9264c7fe
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-07-19 14:47:13 -0500

    add header file that defines a new type: struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct for Poppy
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ * Description: struct that describe some characteristics of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
