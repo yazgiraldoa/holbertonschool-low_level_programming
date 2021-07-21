8b05cca50c143103f9179cca94176f588504442e
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-07-21 01:24:31 -0500

    add function that exec other function with array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - function that executes a function given
+ * as a parameter on each element of an array
+ * @array: pointer to an array
+ * @size: size of the array
+ * @action: pointer to a function that takes an array as paramenter
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+
+}
