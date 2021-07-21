354eb2351d41b78ac497d3da9da3fdfc05e1d553
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-07-21 12:34:52 -0500

    fix function that searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,18 +11,22 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i, result;
-
-	if (size <= 0)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (cmp != NULL && array != NULL)
 	{
-		result = cmp(array[i]);
+		int i, result;
 
-		if (result != 0)
-			return (i);
+		if (size <= 0)
+			return (-1);
+
+		for (i = 0; i < size; i++)
+		{
+			result = cmp(array[i]);
+
+			if (result != 0)
+				return (i);
+		}
+
+		return (-1);
 	}
-
 	return (-1);
 }
