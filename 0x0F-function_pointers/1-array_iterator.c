269b52edcac8d6e903e3c529c810053e3eef4712
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-07-21 12:28:59 -0500

    fix function that exec other function array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	unsigned long int i;
-
-	for (i = 0; i < size; i++)
+	if (action != NULL && array != NULL)
 	{
-		action(array[i]);
-	}
+		unsigned long int i;
 
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
 }
