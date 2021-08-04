fe3c1b0827d30392e8b33b2fc9fe80d61e569cbc
Yazmin Giraldo <edygiraldo.97@gmail.com>
2021-08-03 19:46:56 -0500

    add function that returns the sum of node data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of
+ * all the data (n) of a listint_t linked list.
+ * @head: pointer that points to a structure
+ * Return: sum of data or 0 if list is empty.
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
